eb73121db0a2cd5fadbc2b1d55f479699177a597
muhriddin <muhriddinberdiyev44@gmail.com>
2025-03-29 21:00:16 +0500

    2 kun

--- shop/urls.py
+++ shop/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from shop.views import index
+
+urlpatterns = [
+    path  ("",index, name='index')
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
+# class Index(ListView)
+#     template_name = "shop/index.html"
+
+def index(request):
+    return render(request,"shop/index.html")
